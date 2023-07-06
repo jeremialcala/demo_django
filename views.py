ed023b177d3430fd2f69f2631deda278c9a18454
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-06 15:41:34 -0500

    Configuration changes

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 def data(request):
-    return
+    return JsonResponse(status=200, data={"ResponseCode" : 0, "message": "Process OK"})

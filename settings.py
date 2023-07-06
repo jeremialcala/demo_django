e1a1a9cbaded00cdcadc9336c5c97dc5f12ae379
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-06 17:37:01 -0500

    Services optimization part II Url update

--- settings.py
+++ settings.py
@@ -1,3 +1,2 @@
 # DATABASE
 
-DATABASES = { 'default': { '' }}
--- url.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("admin", admin.site.urls),
-    path("test/data", views.data()),
-    path("test/list", views.list())
-]

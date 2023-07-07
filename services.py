ed6c27f17197287aae1a2531036986269bfe0fe3
Jeremi Alcala <jeremialcala@gmail.com>
2023-07-06 21:20:38 -0500

    creating services and handling some exceptions

--- services.py
+++ services.py
@@ -2,23 +2,27 @@ import requests
 import json
 from models import Framework
 
-url = "https://e-volution.co/django784-test1a1/"
 
-headers = {
-  'Authorization': 'Basic dXNlcjQ1OjU2ZmVkMjM1ZGY0NTNmYmM4MjZkZGMyM2QzZmRiY2FzNzNjNGZz'
-}
+def get_frameworks():
+    frameworks = []
+    url = "https://e-volution.co/django784-test1a1/"
 
-##response = requests.request("GET", url, headers=headers)
+    headers = {
+        'Authorization': 'Basic dXNlcjQ1OjU2ZmVkMjM1ZGY0NTNmYmM4MjZkZGMyM2QzZmRiY2FzNzNjNGZz'
+    }
+    response = requests.request("GET", url, headers=headers)
+    if response.status_code != 200:
+        return frameworks
 
-##data =json.loads(response.text)["data"]
+    data =json.loads(response.text)["data"]
 
-frameworks = []
+    for key in data["frameworks"]:
+        name = key
+        market = data["frameworks"][key]
+        desc = [d for d in data["desc"] if key in d]
+        frameworks.append(Framework(name=name, description=desc, market=market))
 
-##for key in data["frameworks"]:
-##    name = key
-##    market = data["frameworks"][key]
-##    desc = [d for d in data["desc"] if key in d]
-##    frameworks.append(Framework(name=name, description=desc, market=market))
+    return frameworks
 
 
 def get_data():
